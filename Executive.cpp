20d58bb7f4e200b4b8c36cbb508a7f7ae993c710
sampweise <sampweise@gmail.com>
2021-10-25 19:40:22 -0500

    added the win condition the executive class

--- Executive.cpp
+++ Executive.cpp
@@ -1,1 +1,21 @@
+#include "Executive.h"
 
+void Executive::winningCondition(Blackjack* dealer, Blackjack* player)
+{
+    //summing players hand
+    if(player->handValue() > dealer->handValue())
+    {
+        //playyer wins
+        winner = "Player";
+    }
+    else if(player->handValue() == dealer->handValue())
+    {
+        //push
+        winner = "Push";
+    }
+    else
+    {
+        //HOUSE WINS
+        winner = "Dealer"
+    }
+}
--- Executive.h
+++ Executive.h
@@ -1,1 +1,17 @@
+#ifndef EXECUTIVE_H
+#define EXECUTIVE_H
+#include "Blackjack.h"
+#include <string>
 
+class Executive {
+    private:
+        Blackjack* dealer;
+        Blackjack* player;
+        std::string winner;
+
+
+    public:
+        void winningCondition(Blackjack* dealer, Blackjack* player);
+};
+
+#endif
