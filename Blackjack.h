b29ffd99af5ea53d6ccbf585adf09cfa5ef650c5
Tucker <90000281+sizzub@users.noreply.github.com>
2021-10-25 19:26:55 -0500

    added hand management

--- Blackjack.h
+++ Blackjack.h
@@ -1,1 +1,33 @@
+#ifndef BLACKJACK_H
+#define BLACKJACK_H
+#include <string>
+using namespace std;
 
+class Blackjack {
+  
+private: 
+	int[22] hand;
+	int handSize;
+	
+public: 
+	
+	Blackjack(); //initialize hand and handSize
+	
+	int getHandSize();
+	
+	int getCard(int index); //returns the card at the given index (0 to 21) in the hand
+	
+	int handValue(); //returns the optimal hand value for the player (counts aces as 1 if they would cause a bust, 11 otherwise)
+	
+	bool isBust();
+	
+	void hit();
+	
+	int randomCard();
+	
+	string parseCard(int card);
+	
+	int cardValue(int card);
+	
+};
+#endif
