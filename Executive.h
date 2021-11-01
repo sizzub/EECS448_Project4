7a396180bdbbcd79213e070999e50956664b8e23
David0-KU <89867762+David0-KU@users.noreply.github.com>
2021-10-31 21:53:12 -0500

    Java docs

--- Executive.h
+++ Executive.h
@@ -19,12 +19,61 @@ class Executive {
 
     public:
         Executive();
+	
+	/*
+	* @pre Executive initialized
+	* @post runs through the game
+	* @param none
+	* @return none
+	*/
         void run();
+	
+	/*
+	* @pre Executive initialized
+	* @post displays the game information including upto all of the player hand and one card of the dealer hand
+	* @param none
+	* @return none
+	*/
 	void display();
+	
+	/*
+	* @pre Executive initialized
+	* @post displays the game information including upto all of the player hand and all of the dealer hand
+	* @param none
+	* @return none
+	*/	
 	void displayend();
+	
+	/*
+	* @pre Executive initialized
+	* @post displays the game information and informs the player that they bust
+	* @param none
+	* @return none
+	*/	
 	void displaybust();
-        void turnChange();
-        void winningCondition(Blackjack* dealer, Blackjack* player);
+        
+	/*
+	* @pre Executive initialized
+	* @post changes the internal variable currentPlayer to the next player in line 
+	* @param none
+	* @return none
+	*/	
+	void turnChange();
+        
+	/*
+	* @pre Executive initialized
+	* @post calculates the most recent winner and displays and saves the winner
+	* @param the two blackjacks that are checked
+	* @return none
+	*/	
+	void winningCondition(Blackjack* dealer, Blackjack* player);
+	
+	/*
+	* @pre Executive initialized
+	* @post checks if the player wants to quit
+	* @param none
+	* @return 1 if the player wants to continue, 0 if the player wants to quit
+	*/	
 	bool contGame();
 };
 
